ccbae8c229634e4b4d6e7db8eb098e3191483ecc
thanhvn02 <thanhvn@evolly.app>
2024-05-10 15:36:32 +0700

    + add design for exercise 1

---
+++ app/src/main/java/com/example/ex_1_prm392/activity/FormLoginInstagram.java
@@ -0,0 +1,26 @@
+package com.example.ex_1_prm392.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.ex_1_prm392.R;
+
+public class FormLoginInstagram extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_form_login_instagram);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
